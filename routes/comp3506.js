7a8475831823e1315095914dae1b8aa2a3338063
Henry O'Brien <h.obrien17@hotmail.com>
2019-09-01 13:32:35 +1000

    fixing post request bugs

--- routes/comp3506.js
+++ routes/comp3506.js
@@ -6,15 +6,22 @@ router.get('/avltree', function(req, res, next) {
 });
 
 router.post('/avltree/check', function(req, res) {
-    let arr = req.body.arr;
+    let expected = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+    let arr = req.body["arr"];
     while (arr[arr.length - 1] === "") {
         arr.pop();
     }
-    if (arr === ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]) {
-        res.json({"result": true});
-    } else {
-        res.json({"result": true});
+    if (arr.length !== expected.length) {
+        res.send(false);
+        return;
     }
+    for (let i = 0; i < expected.length; i++) {
+        if (arr[i] !== expected[i]) {
+            res.send(false);
+            return;
+        }
+    }
+    res.send(true);
 });
 
 module.exports = router;
